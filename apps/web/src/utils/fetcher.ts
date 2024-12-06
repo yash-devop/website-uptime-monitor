6cff068ca96d9aca4c90179e8081165dc8bd4daa
yash-devop <kamble1234meena@gmail.com>
2024-12-06 22:07:53 +0530

    add: fetchers

---
+++ apps/web/src/utils/fetcher.ts
@@ -0,0 +1,18 @@
+interface FetchError extends Error {
+    statusCode: number,
+    info: any
+}
+export async function fetcher(input: RequestInfo, init: RequestInit){
+    const res = await fetch(input,init);
+
+    if(!res.ok){
+        const error = new Error("An error occured while fetching data") as FetchError;
+
+        error.info = await res.json();
+        error.statusCode = res.status
+
+        throw error;
+    }
+
+    return await res.json();
+}
