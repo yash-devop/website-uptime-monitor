e760c31bc4a7ca1355b8896c6211759fe71a1eab
yash-devop <kamble1234meena@gmail.com>
2025-01-04 17:41:00 +0530

    add: server error handling

--- packages/serverless/src/utils/fetch.ts
+++ packages/serverless/src/utils/fetch.ts
@@ -15,18 +15,17 @@ type FetcherProps = Omit<
   | "checkFrequency"
   | "regions"
 > & {
-  currentRegion: string
+  currentRegion: string;
 };
 
-
 type FetcherReturnType = {
-  success: boolean,
-  response?: Response,
-  error? :{
-    name: string,
-    cause: string
-  }
-}
+  success: boolean;
+  response?: Response;
+  error?: {
+    name: string;
+    cause: string;
+  };
+};
 
 export async function fetcher({
   url,
@@ -34,18 +33,17 @@ export async function fetcher({
   httpRequestBody,
   headerName,
   headerValue,
-  httpRequestTimeout = "1400"
-}: FetcherProps):Promise<FetcherReturnType> {
+  httpRequestTimeout = 1400,
+}: FetcherProps): Promise<FetcherReturnType> {
   const timeoutSignal = AbortSignal.timeout(Number(httpRequestTimeout));
 
   let customHeaders;
 
-  if(headerName && headerValue){
+  if (headerName && headerValue) {
     customHeaders = createHeaders({
-        [headerName]: headerValue,
-        "Content-Type": "application/json",
-    })
-
+      [headerName]: headerValue,
+      "Content-Type": "application/json",
+    });
   }
 
   try {
@@ -55,51 +53,60 @@ export async function fetcher({
       headers: customHeaders ? { ...customHeaders } : {},
       signal: timeoutSignal,
     });
+
+    if (!response.ok) {
+      const error = new Error(response.statusText);
+      error.name = `Status ${response.status}`;
+      throw error;
+    }
     return {
       success: true,
-      response
+      response,
     };
   } catch (err) {
     const error = err as Error;
+    console.log("Error in fetcher catch block:", error);
     if (error.name === "TimeoutError") {
-      console.error(`Timeout: It took more than ${httpRequestTimeout} seconds to get the result!`);
+      console.error(
+        `Timeout: It took more than ${httpRequestTimeout} seconds to get the result!`
+      );
       return {
         success: false,
-        error:{
+        error: {
           name: "HTTP Timeout",
-          cause: JSON.stringify(err)
-        }
-      }
+          cause: error.message,
+        },
+      };
     } else if (error.name === "AbortError") {
       console.error(
         "Fetch aborted by user action (browser stop button, closing tab, etc."
       );
       return {
         success: false,
-        error:{
+        error: {
           name: "HTTP Aborted",
-          cause: JSON.stringify(err)
-        }
-      }
+          cause: error.message,
+        },
+      };
     } else if (error.name === "TypeError") {
-      console.error("DNS Failure" , err);
+      console.error("DNS Failure", err);
       return {
         success: false,
-        error:{
+        error: {
           name: "DNS Failure",
-          cause: JSON.stringify(err)
-        }
-      }
+          cause: error.message,
+        },
+      };
     } else {
       // A network error, or some other problem.
       console.error(`Error: type: ${error}, message: ${error.message}`);
       return {
         success: false,
-        error:{
+        error: {
           name: error.name,
-          cause: JSON.stringify(err)
-        }
-      }
+          cause: error.message,
+        },
+      };
     }
   }
 }
