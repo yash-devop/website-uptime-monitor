9f74956bd29be002c0c2679fb5036016a08c36e6
yash-devop <kamble1234meena@gmail.com>
2025-01-04 17:45:52 +0530

    chore: add zod message in the monitorSchema

--- packages/common/src/schemas/schemas.ts
+++ packages/common/src/schemas/schemas.ts
@@ -1,6 +1,7 @@
 import { z } from "zod";
 // import { type MonitorType , CheckFrequencyType } from "@repo/common";
-import { ALERT_USING, HTTP_METHODS, URL_ALERT_TYPES } from "@repo/db";
+import { ALERT_USING, HTTP_METHODS, URL_ALERT_TYPES } from "../../../db";
+// import { ALERT_USING, HTTP_METHODS, URL_ALERT_TYPES } from "@repo/db";
 import { CHECK_FREQUENCIES } from "../constants/monitor";
 
 // Team Schema
@@ -15,20 +16,40 @@ export const TeamSchema = z.object({
 
 
 export const monitorSchema = z.object({
-    teamId: z.string().cuid(),
-    url: z.string().url(),
-    alertWhen: z.nativeEnum(URL_ALERT_TYPES),
+    teamId: z.string({
+        required_error: "Team ID is required"
+    }),
+    url: z.string({
+        required_error: "Monitoring URL is required"
+    }).url(),
+    alertWhen: z.nativeEnum(URL_ALERT_TYPES , {
+        required_error: "Alert when is required"
+    }),
     alertUsing: z.nativeEnum(ALERT_USING),
-    checkFrequency: z.enum(CHECK_FREQUENCIES),
-    recoveryPeriod: z.number(),
-    confirmationPeriod: z.number(),
+    checkFrequency: z.enum(CHECK_FREQUENCIES , {
+        required_error: "Check Frequency is required"
+    }),
+    recoveryPeriod: z.number({
+        required_error: "Recovery period is required"
+    }),
+    confirmationPeriod: z.number({
+        required_error: "Confirmation period is required"
+    }),
     httpMethods: z.nativeEnum(HTTP_METHODS).optional(),
-    httpRequestTimeout: z.number(),
-    urlAlias: z.string(),
+    httpRequestTimeout: z.number({
+        required_error: "HTTP Request timeout is required"
+    }),
+    urlAlias: z.string({
+        required_error: "Friendly monitor name is required"
+    }).min(4,{
+        message: "Please give a bit more..."
+    }),
     headerName: z.string().optional(),
     headerValue: z.string().optional(),
     httpRequestBody: z.string().optional(),
-    regions: z.string().array()
+    regions: z.string({
+        required_error: "Monitoring URL is required"
+    }).array()
 })
 
 // Invitation Schema
