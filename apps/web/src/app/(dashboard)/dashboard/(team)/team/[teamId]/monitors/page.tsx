30ed83187c3b7f718f971865f01b211a777b5129
yash-devop <kamble1234meena@gmail.com>
2025-01-05 18:26:11 +0530

    add: monitors page with displayrow component

--- apps/web/src/app/(dashboard)/dashboard/(team)/team/[teamId]/monitors/page.tsx
+++ apps/web/src/app/(dashboard)/dashboard/(team)/team/[teamId]/monitors/page.tsx
@@ -1,5 +1,13 @@
+import DisplayRow, {
+  DisplayLeftElement,
+  DisplayRightElement,
+  DisplayRowLeftSection,
+  DisplayRowRightSection,
+  IconGenerator,
+} from "@/app/components/DisplayRow";
 import { Button } from "@/app/components/ui/button";
 import { auth } from "@/utils/auth";
+import { prisma } from "@repo/db";
 import { CircleDashed, CircleDot, Ellipsis } from "lucide-react";
 import { headers } from "next/headers";
 import Link from "next/link";
@@ -7,30 +15,36 @@ import Link from "next/link";
 export default async function MonitorPage({
   params,
 }: {
-  params: {
+  params: Promise<{
     teamId: string;
-  };
+  }>;
 }) {
   const headerList = headers();
   const pathname = headerList.get("x-current-path");
   const session = await auth();
+  const { teamId } = await params;
   console.log("Path in Page : ", pathname);
 
-  const arr = [
-    {
-      name: "httpstat.us",
+  const monitors = await prisma.monitor.findMany({
+    where: {
+      teamId,
     },
-    {
-      name: "httpstat.us",
+    include: {
+      CheckLog: {
+        orderBy: {
+          isUp: "asc",
+        },
+        take: 1,
+      },
     },
-  ];
+  });
 
   return (
     <>
       <div className="w-full">
-        <div className="flex flex-col md:flex-row items-start md:items-center w-full justify-between">
+        <div className="flex flex-col md:flex-row items-start md:items-center w-full justify-between gap-4">
           <p className="text-2xl capitalize"> Hello , {session?.user?.name} </p>
-          <Button size={"sm"}>
+          <Button size={"sm"} className="">
             <Link href={`${pathname}/create`}>Create monitor</Link>
           </Button>
         </div>
@@ -38,65 +52,54 @@ export default async function MonitorPage({
         <div className="flex flex-col gap-4 pt-8 overflow-x-auto w-full bg-bslack-4">
           {/* Wrap the rows in a container */}
           <div className="flex flex-col min-w-max">
-            {arr.map(({ name }, idx) => (
-              <MonitorRow idx={idx} name={name} length={arr.length} pathname={pathname!}/>
-            ))}
+            {monitors.length > 0 ? (
+              monitors.map(({ urlAlias, id, CheckLog }, idx) => (
+                // TODO: add isUp or monitorUp or down so to give color to the DisplayRow's Dot / fancy icon
+                <DisplayRow idx={idx} monitorId={id} pathname={pathname!} length={monitors.length}>
+                  <DisplayRowLeftSection className="gap-9">
+                    <DisplayLeftElement>
+                      <IconGenerator
+                        iconType={CheckLog.length > 0 ? "dot" : "dot"}
+                        heartBeatType={"once"}
+                        className={CheckLog.length > 0 ? (CheckLog[0].webStatus === "down" ? "bg-red-400 text-red-400" : "") : "bg-neutral-5 text-neutral-5" }
+                      />
+                    </DisplayLeftElement>
+                    <DisplayLeftElement>
+                      <div className="flex flex-col">
+                        <h2 className="text-sm">{"name"}</h2>
+                        <p className="text-neutral-3 text-xs">
+                          <span className="text-red-400">Down </span>2 days ago
+                        </p>
+                      </div>
+                    </DisplayLeftElement>
+                  </DisplayRowLeftSection>
+
+                  <DisplayRowRightSection className="gap-x-10">
+                    <DisplayRightElement>
+                      <div className="text-sm text-neutral-3 flex items-center gap-2 w-fit">
+                        <CircleDot size={16} className="text-neutral-4" />
+                        <p>Checking every {"3m"}</p>
+                      </div>
+                    </DisplayRightElement>
+                    <DisplayRightElement>
+                      <div className="pr-4">
+                        <div className="hover:bg-neutral-6/50 p-1 rounded-md cursor-pointer">
+                          <Ellipsis size={17} className="text-neutral-4" />
+                        </div>
+                      </div>
+                    </DisplayRightElement>
+                    
+                  </DisplayRowRightSection>
+                </DisplayRow>
+              ))
+            ) : (
+              <div className="text-neutral-5 w-full flex items-center pt-12 justify-center">
+                <p>Ahhh... No monitors found</p>
+              </div>
+            )}
           </div>
         </div>
       </div>
-    </>
-  );
-}
-
-function MonitorRow({
-  idx,
-  name,
-  length,
-  pathname
-}: {
-  idx: number;
-  name: string;
-  length: number;
-  pathname: string
-}) {
-  return (
-    <>
-        <Link href={`${pathname}/create`}>
-            <div
-            className={`flex justify-between items-center py-3 gap-4 bg-neutral-7/40 border-sidebar-border/60 p-2 overflow-x-auto w-full hover:bg-neutral-7/90 transition-all ${
-                length === 0
-                ? "border rounded-lg "
-                : idx === 0
-                    ? length > 1
-                    ? "border border-b-0 rounded-t-lg rounded-tr-lg rounded-tl-lg"
-                    : "border rounded-lg"
-                    : idx === length - 1
-                    ? "border rounded-br-lg rounded-bl-lg "
-                    : "border-l border-r border-t "
-            } `}
-            >
-            <div className="pl-4 pr-2">
-                <div className="size-3 bg-green-1 rounded-full duration-[4000]" />
-            </div>
-            <div className="grow flex items-center justify-between pr-5 lg:pr-16">
-                <div className="grow">
-                    <h2 className="text-sm">httpstat.us</h2>
-                    <p className="text-neutral-3 text-xs">
-                        <span className="text-red-400">Down </span>2 days ago
-                    </p>
-                </div>
-                <div className="text-sm text-neutral-3 flex items-center gap-1">
-                    <CircleDot size={16} className="text-neutral-4"/>
-                    <p>3m</p>
-                </div>
-            </div>
-            <div className="pr-4">
-                <div className="hover:bg-neutral-6/50 p-1 rounded-md cursor-pointer">
-                        <Ellipsis size={17} className="text-neutral-4" />
-                </div>
-            </div>
-            </div>
-        </Link>
     </>
   );
 }
