2f3b76034004fb9f3fd5ecbc6167c6fc3893862f
yash-devop <kamble1234meena@gmail.com>
2025-01-05 18:23:23 +0530

    add: shadcn component

--- apps/web/src/app/components/ui/input.tsx
+++ apps/web/src/app/components/ui/input.tsx
@@ -11,7 +11,7 @@ const Input = React.forwardRef<HTMLInputElement, InputProps>(
       <input
         type={type}
         className={cn(
-          "flex h-10 w-full rounded-md border border-input bg-background px-3 py-2 text-sm ring-offset-background file:border-0 file:bg-transparent file:text-sm file:font-medium file:text-foreground placeholder:text-muted-foreground focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-50",
+          "flex h-10 w-full rounded-md border border-neutral-6 bg-background px-3 py-2 text-sm placeholder:text-neutral-5 ring-offset-background file:border-0 file:bg-transparent file:text-sm file:font-medium file:text-foreground focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-offset-4 focus-visible:border-primary focus-visible:ring-transparent focus-visible:ring-offset-primary/10 disabled:cursor-not-allowed disabled:opacity-50",
           className
         )}
         ref={ref}
