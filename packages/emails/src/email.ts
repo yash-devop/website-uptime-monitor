d5baf24e8dcac514d2e11e80570e2d09cee4e611
yash-devop <kamble1234meena@gmail.com>
2025-01-11 11:39:27 +0530

    add: sendEmail to send emails using nodemailer

--- packages/emails/src/email.ts
+++ packages/emails/src/email.ts
@@ -1,21 +1,22 @@
-import { createTransport , SendMailOptions } from "nodemailer"
-
+import { createTransport, SendMailOptions } from "nodemailer";
+import { config } from "dotenv";
+config();
 
 interface MailOptions {
-    from: SendMailOptions["from"],
-    to: SendMailOptions["to"],
-    subject: SendMailOptions["subject"],
-    html: SendMailOptions["html"]
+  from: SendMailOptions["from"];
+  to: SendMailOptions["to"];
+  subject: SendMailOptions["subject"];
+  html: SendMailOptions["html"];
 }
 
 export const sendEmail = async (options: MailOptions) => {
-    console.log('process.env.UPTIME_APP_EMAIL',process.env.UPTIME_APP_EMAIL);
-    createTransport({
-        host: "smtp-mail.outlook.com",
-        service: "gmail",
-        auth: {
-            user: process.env.UPTIME_APP_EMAIL,
-            pass: process.env.UPTIME_APP_PASSWORD   
-        }
-    }).sendMail(options)
-}
+  console.log("process.env.UPTIME_APP_EMAIL", process.env.UPTIME_APP_EMAIL);
+  createTransport({
+    host: "smtp-mail.outlook.com",
+    service: "gmail",
+    auth: {
+      user: process.env.UPTIME_APP_EMAIL,
+      pass: process.env.UPTIME_APP_PASSWORD,
+    },
+  }).sendMail(options);
+};
