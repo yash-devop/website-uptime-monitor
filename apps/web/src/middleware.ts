377503690d9f08f5ae0e81e1b98d789caccbc3ce
yash-devop <kamble1234meena@gmail.com>
2025-01-17 18:34:40 +0530

    chore: remove unused imports

--- apps/web/src/middleware.ts
+++ apps/web/src/middleware.ts
@@ -1,8 +1,8 @@
 import { chain } from "./middlewares/chain";
 import { withAuthMiddleware } from "./middlewares/withAuthMiddleware";
 import { withPathnameMiddleware } from "./middlewares/withPathnameMiddleware";
-import { createTeamMiddleware } from "./middlewares/createTeamMiddleware";
-import { testingMiddleware } from "./middlewares/testingMiddleware";
+// import { createTeamMiddleware } from "./middlewares/createTeamMiddleware";
+// import { testingMiddleware } from "./middlewares/testingMiddleware";
 
 export default chain([withAuthMiddleware , withPathnameMiddleware])
 // export default chain([withAuthMiddleware , withPathnameMiddleware , createTeamMiddleware , testingMiddleware])
