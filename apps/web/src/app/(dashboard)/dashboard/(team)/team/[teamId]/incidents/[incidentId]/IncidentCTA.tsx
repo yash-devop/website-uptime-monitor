8d9b4168219aa74eb2e43440ac09a5dd15fc6fea
yash-devop <kamble1234meena@gmail.com>
2025-01-17 13:31:48 +0530

    add: incident cta component

---
+++ apps/web/src/app/(dashboard)/dashboard/(team)/team/[teamId]/incidents/[incidentId]/IncidentCTA.tsx
@@ -0,0 +1,61 @@
+"use client";
+
+import { cn } from "@/lib/utils";
+import { LucideProps, Settings, ShieldAlert, Trash2 } from "lucide-react";
+import Link from "next/link";
+import { useParams, usePathname } from "next/navigation";
+
+export const IncidentCTA = () => {
+  const pathname = usePathname();
+  const {teamId} = useParams()
+  return (
+    <>
+      <div className="flex flex-wrap items-center gap-3 pt-6">
+      <QuickCTA
+          href={pathname.split("incidents")[0] + "monitors/"}
+          Icon={ShieldAlert}
+          name="View Monitor"
+          className="hover:bg-blue-500/10 hover:text-blue-500"
+        />
+        <QuickCTA
+          href=""
+          Icon={Trash2}
+          name="Remove"
+          className="hover:bg-red-500/10 hover:text-red-500"
+          onClick={async () => {
+            // TODO: attach the api of delete monitor
+            alert("Sheesh");
+          }}
+        />
+      </div>
+    </>
+  );
+};
+
+const QuickCTA = ({
+  Icon,
+  name,
+  className,
+  href,
+  onClick
+}: {
+  Icon: React.ComponentType<LucideProps>;
+  name: string;
+  className?: string;
+  href?: string;
+  onClick?:()=>void
+}) => {
+  return (
+    <Link
+      href={href ?? ""}
+      className={cn(
+        `flex items-center gap-2 px-2 py-1.5 text-sm hover:bg-neutral-7 rounded-md select-none cursor-pointer transition-all text-neutral-4`,
+        className
+      )}
+      onClick={onClick}
+    >
+      <Icon size={16} />
+      <span className="text-balance">{name}</span>
+    </Link>
+  );
+};
