7477497ff5f10072c23d21812db96b9d313e8a80
yash-devop <kamble1234meena@gmail.com>
2025-01-17 13:31:06 +0530

    add: incident by id page

--- apps/web/src/app/(dashboard)/dashboard/(team)/team/[teamId]/incidents/[incidentId]/page.tsx
+++ apps/web/src/app/(dashboard)/dashboard/(team)/team/[teamId]/incidents/[incidentId]/page.tsx
@@ -4,7 +4,10 @@ import { Button } from "@/app/components/ui/button";
 import { Globe, Trash2 } from "lucide-react";
 import ClientSideTabs from "./ClientSideTabs";
 import { Metadata, ResolvingMetadata } from "next";
-
+import { INCIDENT_STATUS, prisma } from "@repo/db";
+import Link from "next/link";
+import { format } from "date-fns";
+import { IncidentCTA } from "./IncidentCTA";
 
 // 1. Static Metadata
 // export const metadata:Metadata = {
@@ -12,92 +15,171 @@ import { Metadata, ResolvingMetadata } from "next";
 // }
 
 // 2. Dynamic metadata
-// export async function generateMetadata(
-//     parent: ResolvingMetadata
-//   ): Promise<Metadata> {
-//     const value = Math.random()
-//     return {
-//       title: value.toString(),
-//       icons: {
-//         icon: { url: "https://m.youtube.com/static/logos/favicon.ico" }
-//       },
-//     }
-// }
-export default async function MonitorIDPage() {
+// => green icon = ../../../../../../../upfavicon.ico
+// => red icon = ../../../../../../../downfavicon.ico
+// => testing favicon = "https://m.youtube.com/static/logos/favicon.ico"
+export async function generateMetadata(
+  {
+    params
+  }:{
+    params: Promise<{
+      incidentId: string
+    }>,
+  },
+  parent: ResolvingMetadata,
+): Promise<Metadata> {
+  console.log('params in METATAG',await params);
+  const {incidentId} = await params
+  const incident = await prisma.incident.findFirst({
+    where: {
+      id: incidentId,
+    },
+    include:{
+      monitor:{
+        select:{
+          httpMethods: true
+        }
+      }
+    }
+  });
+  return {
+    title: incident?.url,
+    icons: {
+      icon: { url: incident?.incidentStatus === "ongoing" ? "../../../../../../../downfavicon.ico" : "../../../../../../../upfavicon.ico" },
+    },
+  };
+}
+
+export default async function MonitorIDPage({
+  params,
+}: {
+  params: Promise<{
+    incidentId: string;
+    teamId: string;
+  }>;
+}) {
+  const { incidentId } = await params;
+  const incident = await prisma.incident.findFirst({
+    where: {
+      id: incidentId,
+    },
+    include:{
+      monitor:{
+        select:{
+          httpMethods: true
+        }
+      }
+    }
+  });
+  console.log("incident", incident);
+
+  const Incident_status: Record<
+    INCIDENT_STATUS,
+    { value: string }
+  > = {
+    ongoing: {
+      // title: "Started on",
+      value: "Ongoing",
+    },
+    validating: {
+      // title: "Validation started on",
+      value: "Acknowledged",
+    },
+    resolved: {
+      // title: "Resolved at",
+      value: incident?.resolvedAt?.toString() || "---",
+    },
+  };
   return (
     <>
-      <div className="flex flex-col gap-4 max-w-[1400px] mx-auto w-full">
-        <div className="flex flex-wrap items-center justify-between gap-3">
-          <div className="flex items-center gap-3">
-            <IconGenerator
-              iconType="fancy"
-              color="size-12 rounded-lg"
-              iconSize="size-7"
-            />
-            <div>
-              <h1 className="text-2xl font-semibold">httpstat.us/495</h1>
-              <p className="text-red-400">
-                Ongoing
-                <span className="text-neutral-4">
-                  {" "}
-                  · {"4 Jan 2025 at 10:08am IST"}
-                </span>
-              </p>
-            </div>
-          </div>
-          <div className="flex items-center gap-4 pt-4">
-            <Button className="bg-neutral-5 border-none hover:bg-neutral-6 font-semibold">
-              Escalate
-            </Button>
-            <Button>Acknowledge</Button>
-          </div>
-        </div>
-        <div className="flex flex-wrap items-center justify-between gap-3 py-6">
-          <div className="flex items-center gap-5">
-            <div className="flex items-center gap-2 px-3 py-2 hover:bg-neutral-7 rounded-md select-none cursor-pointer transition-all text-neutral-4">
-              <Globe size={18} />
-              <span className="text-balance">Monitor</span>
-            </div>
-            <div className="flex items-center gap-2 px-3 py-2 hover:bg-neutral-7 rounded-md select-none cursor-pointer transition-all text-neutral-4">
-              <Trash2 size={18} />
-              <span className="text-balance">Remove</span>
-            </div>
-          </div>
-          <div className="flex items-center gap-4">
-            <Badge
-              variant={"secondary"}
-              className="bg-neutral-6 hover:bg-neutral-6 text-neutral-3 flex items-center gap-1 truncate px-3"
-            >
-              Acknowledged by
-              <div className="flex items-center gap-1">
-                <img
-                  src="https://pbs.twimg.com/profile_images/77846223/profile_400x400.jpg"
-                  className="size-5 rounded-full"
-                />
-                <span className="truncate">Yash Kamble</span>
+      {incident ? (
+        <div className="flex flex-col gap-4 max-w-[1400px] mx-auto w-full">
+          <div className="flex flex-wrap items-center justify-between gap-3">
+            <div className="flex items-center gap-3">
+              <IconGenerator
+                iconType="fancy"
+                color="size-11 rounded-lg"
+                iconSize="size-6"
+              />
+              <div>
+                <h1 className="text-xl font-semibold">
+                  {incident.incidentName}
+                </h1>
+                <p className="text-red-400 text-sm">
+                  {incident.incidentStatus}
+                  <span className="text-neutral-4">
+                    {" "}
+                    · {format(incident.createdAt, "dd MMM yyyy 'at' hh:ss a")}
+                  </span>
+                </p>
               </div>
-            </Badge>
+            </div>
+            <div className="flex items-center gap-4 pt-4">
+              <Button>Acknowledge</Button>
+            </div>
           </div>
-        </div>
-        <div className="grid grid-cols-1 lg:grid-cols-3 gap-4">
-          <StatusCard name="Cause" status="Status 495" />
-          <StatusCard name="Started" status="27 minutes ago" />
-          <StatusCard name="Length" status="Acknowledged" />
-        </div>
-        <div className=" bg-neutral-7  border border-neutral-6 rounded-lg overflow-hidden">
-          <div className="px-6 py-5 min-h-[50px] flex flex-col gap-2">
-            <span className="text-neutral-3 text-sm">Checked URL</span>
-            <span className="text-sm tracking-wide font-light text-white">
-              GET {`https://httpstat.us/495`}
-            </span>
+          <div className="flex flex-wrap items-center justify-between gap-3 py-6">
+            <div className="flex items-center gap-5">
+              {/* <div className="flex items-center gap-2 px-3 py-2 hover:bg-neutral-7 rounded-md select-none cursor-pointer transition-all text-neutral-4">
+                <Globe size={18} />
+                <span className="text-balance">Monitor</span>
+              </div>
+              <div className="flex items-center gap-2 px-3 py-2 hover:bg-neutral-7 rounded-md select-none cursor-pointer transition-all text-neutral-4">
+                <Trash2 size={18} />
+                <span className="text-balance">Remove</span>
+              </div> */}
+              <IncidentCTA />
+            </div>
+            {
+              (incident.acknowledgedBy || incident.incidentStatus === "validating") ? (
+                  <div className="flex items-center gap-4">
+                    <Badge
+                      variant={"secondary"}
+                      className="bg-neutral-6 hover:bg-neutral-6 text-neutral-3 flex items-center gap-1 truncate px-3"
+                    >
+                      Acknowledged by
+                      <div className="flex items-center gap-1">
+                        <span className="truncate">{incident.acknowledgedBy || "Unavailable"}</span>
+                      </div>
+                    </Badge>
+                  </div>
+              ) : null
+            }
           </div>
-          <div className="px-6 py-5 bg-neutral-800 flex flex-col gap-2">
-            <span className="text-neutral-3 text-sm">Escalation</span>
-            <span className="text-white">Entire Team</span>
+          <div className="grid grid-cols-1 lg:grid-cols-3 gap-4">
+            <StatusCard name="Cause" status={incident.incidentName} />
+            <StatusCard name={"Started"} status="10 Jan 2025 at 10pm" />
+            <StatusCard name="Status" status={Incident_status[incident.incidentStatus].value} />
           </div>
+          <div className=" bg-neutral-7  border border-neutral-6 rounded-lg overflow-hidden">
+            <div className="px-6 py-5 min-h-[50px] flex flex-col gap-2">
+              <span className="text-neutral-3 text-sm">Checked URL</span>
+              <span className="text-sm tracking-wide font-light text-white">
+                {incident.monitor.httpMethods.toUpperCase()} {incident.url}
+              </span>
+            </div>
+            <div className="px-6 py-5 bg-neutral-800 flex flex-col gap-2">
+              <span className="text-neutral-3 text-sm">Escalation</span>
+              <span className="text-white">Entire Team</span>
+            </div>
+          </div>
+          <ClientSideTabs status={incident.incidentCause} url={incident.url}/>
         </div>
-        <ClientSideTabs />
-      </div>
+      ) : (
+        <>
+          <div className="flex flex-col items-center justify-center h-full">
+            <h1 className="text-2xl font-semibold text-red-500">
+              Monitor Not Found
+            </h1>
+            <p className="text-gray-500">
+              The requested monitor does not exist or could not be retrieved.
+            </p>
+            <Link href="/incidents" className="mt-4 text-blue-500 underline">
+              Go back to Monitors
+            </Link>
+          </div>
+        </>
+      )}
     </>
   );
 }
@@ -105,9 +187,9 @@ export default async function MonitorIDPage() {
 const StatusCard = ({ name, status }: { name: string; status: string }) => {
   return (
     <>
-      <div className="bg-neutral-7 border border-neutral-6 rounded-lg min-h-[90px] flex flex-col gap-1.5 p-6">
+      <div className="bg-neutral-7 border border-neutral-6 rounded-lg min-h-[80px] flex flex-col gap-1.5 p-6">
         <span className="text-sm text-neutral-3">{name}</span>
-        <span className="text-2xl">{status}</span>
+        <span className="text-xl">{status}</span>
       </div>
     </>
   );
