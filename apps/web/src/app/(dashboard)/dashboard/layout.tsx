37d244e8e9feb867703e48c3461035a0ae0bdb29
yash-devop <kamble1234meena@gmail.com>
2025-01-17 18:36:10 +0530

    rm: breadcrumbs

--- apps/web/src/app/(dashboard)/dashboard/layout.tsx
+++ apps/web/src/app/(dashboard)/dashboard/layout.tsx
@@ -1,66 +1,24 @@
 import { AppSidebar } from "@/app/components/AppSidebar";
 import ClientSideTriggerButton from "@/app/components/ClientSideTriggerButton";
 import { SidebarProvider } from "@/app/components/ui/sidebar";
-import BreadcrumbSlot from "@/app/components/BreadcrumbSlot";
-import { cookies, headers } from "next/headers";
-
-type BreadcrumbElements = { [key: string]: { label: string; url: string }[] };
-
 
 export default function DashboardLayout({
   children,
 }: {
   children: React.ReactNode;
 }) {
-  const BREADCRUMB_HEADER_HEIGHT = "45px"
-  const cookieStore = cookies()
-  const pathname = headers().get("x-current-path")!;
-  const teamSlug = cookieStore.get("team-slug")?.value ?? pathname.split("/")[3]
 
-  const teamRoute = '/dashboard/team';
-  const breadcrumbConfig: BreadcrumbElements = {
-    "^/dashboard/team/[\\w-]+/monitors/create$": [
-      { label: "Monitors", url: `${teamRoute}/${teamSlug}/monitors` },
-      { label: "Create Monitor", url: `${teamRoute}/${teamSlug}/monitors/create` },
-    ],
-    "^/dashboard/team/[\\w-]+/incidents$": [
-      { label: "Dashboard", url: "/dashboard" },
-      { label: "Incidents", url: `${teamRoute}/${teamSlug}/incidents` },
-    ],
-    "^/dashboard/team/[\\w-]+/status$": [
-      { label: "Dashboard", url: "/dashboard" },
-      { label: "Status", url: `${teamRoute}/${teamSlug}/status` },
-    ],
-  };
-  const breadcrumbItems = Object.keys(breadcrumbConfig).reduce(
-    (matchedItems, pattern) => {
-      const regex = new RegExp(pattern);
-      return regex.test(pathname) ? breadcrumbConfig[pattern] : matchedItems;
-    },
-    undefined as { label: string; url: string }[] | undefined
-  );
-
-  console.log('breadcrumbItems',breadcrumbItems);
   return (
     <div className="min-h-screen h-full w-full overflow-auto">
       <SidebarProvider>
         <AppSidebar/>
         {/* <AppSidebar pathname={pathname} teamId={teamSlug}/> */}
-        <main className="w-full">
+        <main className="w-full overflow-auto">
           <div className="p-2">
             <ClientSideTriggerButton />
           </div>
           <div className="w-full">
-            {
-              typeof(breadcrumbItems) !== "undefined" ? (
-                <div style={{
-                  height: BREADCRUMB_HEADER_HEIGHT
-                }} className="my-3 w-full border-b border-sidebar-border px-4 overflow-x-auto">
-                  <BreadcrumbSlot elements={breadcrumbItems} />
-                </div>
-              ) : null
-            }
-            <div className="py-8 px-6 md:py-10 md:px-20">
+            <div className="py-8 px-6 md:py-10 md:px-14 lg:px-20">
               {children}
             </div>
           </div>
