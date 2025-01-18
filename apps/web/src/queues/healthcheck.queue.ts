1658e168b343533d35b157f02e22a4a0ef2a8e7e
yash-devop <kamble1234meena@gmail.com>
2025-01-18 20:17:18 +0530

    add: upstash endpoint

--- apps/web/src/queues/healthcheck.queue.ts
+++ apps/web/src/queues/healthcheck.queue.ts
@@ -4,6 +4,9 @@ export const HealthCheckQueue = BullQueue({
   queueName: "HealthCheckQueue",
   connection: {
     disconnectTimeout: 500,
-    commandTimeout: 500
+    commandTimeout: 500,
+    url: process.env.UPSTASH_REDIS_REST_URL,
+    password: process.env.UPSTASH_REDIS_REST_TOKEN,
+    port: 6379
   },
 });
