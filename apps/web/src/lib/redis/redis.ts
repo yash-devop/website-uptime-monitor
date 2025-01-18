edf21800034028a9b1eb18689fc58c61b5fb0543
yash-devop <kamble1234meena@gmail.com>
2025-01-18 09:30:21 +0530

    add: upstash redis

--- apps/web/src/lib/redis/redis.ts
+++ apps/web/src/lib/redis/redis.ts
@@ -1,12 +1,12 @@
 // import Redis from 'ioredis'
 import {Redis} from '@upstash/redis'
-const getRedisUrl=()=>{
-    console.log('UPSTASH_REDIS_URL',process.env.UPSTASH_REDIS_URL);
-    if(process.env.UPSTASH_REDIS_URL){
-        console.log('yes redis url is there.');
-        return process.env.UPSTASH_REDIS_URL
-    }
-    throw new Error("UPSTASH_REDIS_URL is not defined")
-}
+// const getRedisUrl=()=>{
+//     console.log('UPSTASH_REDIS_URL',process.env.UPSTASH_REDIS_URL);
+//     if(process.env.UPSTASH_REDIS_URL){
+//         console.log('yes redis url is there.');
+//         return process.env.UPSTASH_REDIS_URL
+//     }
+//     throw new Error("UPSTASH_REDIS_URL is not defined")
+// }
 
 export const redis = Redis.fromEnv()
